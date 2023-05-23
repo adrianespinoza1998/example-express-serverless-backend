626678bd44b6a79f8ae1a91701e7523978b31d03
Adrian Espinoza <adrianespinozaarevalo@gmail.com>
2023-05-23 14:48:14 -0400

    add test get customer serices

--- test/customerServices.unit.test.js
+++ test/customerServices.unit.test.js
@@ -4,13 +4,22 @@ describe("CustomerServices", () => {
   beforeAll(async () => {
     this.customerServices = new CustomerServices();
   });
+
   it("should create a customer", async () => {
     const name = "test";
-    const email = "TEST@TEST    ";
+    const email = "TEST@TEST";
 
     const isCreated = await this.customerServices.createCustomer(name, email);
 
     expect(isCreated).toBe(true);
   });
-  //   it("should get all customers", async () => {});
+
+  it("should get all customers", async () => {
+    const customers = await this.customerServices.getCustomers();
+
+    expect(customers).toBeDefined();
+    expect(customers.Items.length).toBeGreaterThan(0);
+    expect(customers.Items[0].primary_key).toBeDefined();
+    expect(customers.Items[0].email).toBeDefined();
+  });
 });
