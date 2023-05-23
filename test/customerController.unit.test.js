0d9d007de04792f4a0b997311d8fd7ce65e1c5c2
Adrian Espinoza <adrianespinozaarevalo@gmail.com>
2023-05-23 15:08:42 -0400

    add get all customers controller test

--- test/customerController.unit.test.js
+++ test/customerController.unit.test.js
@@ -13,4 +13,16 @@ describe("CustomerController", () => {
     expect(response.status).toBe(201);
     expect(response.body).toEqual({ name, email });
   });
+
+  it("should get all customers", async () => {
+    const response = await request(app).get("/customers");
+
+    expect(response.status).toBe(200);
+    expect(response.body).toBeDefined();
+    expect(response.body.data.length).toBeGreaterThan(0);
+    expect(response.body.data[0].name).toBeDefined();
+    expect(typeof response.body.data[0].name).toBe("string");
+    expect(response.body.data[0].email).toBeDefined();
+    expect(typeof response.body.data[0].name).toBe("string");
+  });
 });
--- test/customerServices.unit.test.js
+++ test/customerServices.unit.test.js
@@ -20,6 +20,8 @@ describe("CustomerServices", () => {
     expect(customers).toBeDefined();
     expect(customers.Items.length).toBeGreaterThan(0);
     expect(customers.Items[0].primary_key).toBeDefined();
+    expect(typeof customers.Items[0].primary_key).toBe("string");
     expect(customers.Items[0].email).toBeDefined();
+    expect(typeof customers.Items[0].email).toBe("string");
   });
 });
